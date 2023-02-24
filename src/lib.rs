f44ca41ea0fdffdba7f1a939683eb18bd0c58e60
kaleab <808kaleabkzin@gmail.com>
2023-02-24 22:58:48 +0300

    http methods

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// routes module
+mod routes;
+
+pub async fn run() {
+    let app = routes::create_routes();
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
-use axum::{routing::get, Router};
-
-// async fn hello_world() -> &'static str {
-//     "hello world"
-// }
+// from lib.rs
+use hello_world::run;
 
 #[tokio::main]
 async fn main() {
-    // let app = Router::new().route("/", get(hello_world));
-
-    let app = Router::new().route("/", get(|| async { "hello again" }));
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    run().await;
 }
